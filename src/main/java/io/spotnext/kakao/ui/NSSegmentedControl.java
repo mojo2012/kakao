5aca28833f599a6d1e2da2aaac664f21926e6cf0
Matthias Fuchs <meister.fuchs@gmail.com>
2019-06-26 20:53:20 +0200

    more toolbar items

---
+++ src/main/java/io/spotnext/kakao/ui/NSSegmentedControl.java
@@ -0,0 +1,9 @@
+package io.spotnext.kakao.ui;
+
+public class NSSegmentedControl extends NSView {
+
+	public NSSegmentedControl() {
+		super("NSSegmentedControl");
+	}
+
+}
---
+++ src/main/java/io/spotnext/kakao/ui/NSToolbarItemGroup.java
@@ -0,0 +1,38 @@
+package io.spotnext.kakao.ui;
+
+import java.util.ArrayList;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import ca.weblite.objc.Proxy;
+import io.spotnext.kakao.foundation.NSSize;
+import io.spotnext.kakao.structs.NSMutableArray;
+
+public class NSToolbarItemGroup extends NSToolbarItem {
+
+	public NSToolbarItemGroup(String identifier) {
+		super(identifier, "NSToolbarItemGroup");
+	}
+
+	public void setSubitems(NSToolbarItem... items) {
+		if (ArrayUtils.isNotEmpty(items)) {
+			var nativeHandles = new ArrayList<Proxy>();
+
+			for (var item : items) {
+				nativeHandles.add(item.getNativeHandle());
+			}
+
+			var array = new NSMutableArray<>(Proxy.class);
+			array.addObjects(nativeHandles);
+
+			for (var item : items) {
+				item.setMinSize(new NSSize(40, 40));
+				item.setMaxSize(new NSSize(80, 40));
+			}
+			
+//			var arrayProxy = new Proxy()
+
+			nativeHandle.send("setSubitems:", array.getNativeHandle());
+		}
+	}
+}
